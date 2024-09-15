ce4590e379f18dd3766ef478c1f1b31a8940a5a8
thomas <bezille.thomasb@gmail.com>
2024-09-15 10:11:54 +0200

    📦 NEW: Creation of the function for managing the victory condition

--- index.py
+++ index.py
@@ -13,6 +13,18 @@ def draw() -> None:
             print(map[i][j], end="")
         print()
 
+def check_win() -> bool:
+    '''Check if a player has won the game by checking the rows, columns and diagonals'''
+    for i in range(3):
+       if map[i][0] == map[i][1] == map[i][2] != " . ": # Check rows
+           return True
+    for i in range(3):
+       if map[0][i] == map[1][i] == map[2][i] != " . ": # Check columns
+           return True
+    if map[0][0] == map[1][1] == map[2][2] != " . " or map[0][2] == map[1][1] == map[2][0] != " . ": # Check diagonals
+        return True
+    return False
+
 draw()
 
 while True:
